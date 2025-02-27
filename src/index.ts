344209814dcd77c99ed3b7beda4dd9f595d76113
Arief Budiman <mariefbudiman12@gmail.com>
2025-02-27 09:50:37 +0700

    add data

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ const app: Application = express();
 const port = 4000;
 
 app.use("/health", (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ status: "200" });
+    res.status(200).send({ status: "200", data: "Hello World" });
 });
 
 app.listen(port, () => console.log(`Server is listening on port ${port}`));
