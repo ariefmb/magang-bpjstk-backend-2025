8c9832d8b92cbe51e1d9a1f3e8fa7a36312c70e3
Arief Budiman <mariefbudiman12@gmail.com>
2025-02-27 09:48:17 +0700

    initialize project

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+// const express = require("express")
+import express, { Application, Request, Response, NextFunction } from "express"; // karena pakai typescript, bisa menggunakan import
+
+const app: Application = express();
+const port = 4000;
+
+app.use("/health", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).send({ status: "200" });
+});
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`));
