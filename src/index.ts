f8f8a52d59ed1b66037bd90acf7e0601eae74719
Arief Budiman <mariefbudiman12@gmail.com>
2025-02-27 11:06:12 +0700

    add eslintrc and prettierrc

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 // const express = require("express")
-import express, { Application, Request, Response, NextFunction } from "express"; // karena pakai typescript, bisa menggunakan import
+import express, { Application, Request, Response, NextFunction } from 'express' // karena pakai typescript, bisa menggunakan import
 
-const app: Application = express();
-const port = 4000;
+const app: Application = express()
+const port = 4000
 
-app.use("/health", (req: Request, res: Response, next: NextFunction) => {
-    res.status(200).send({ status: "200", data: "Hello World" });
-});
+app.use('/health', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ status: '200', data: 'Hello World' })
+})
 
-app.listen(port, () => console.log(`Server is listening on port ${port}`));
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
