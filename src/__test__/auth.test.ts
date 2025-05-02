9af90f66132329ce62cd0ccc1a9140f37f064bd6
Arief Budiman <mariefbudiman12@gmail.com>
2025-05-02 13:54:21 +0700

    add unit testing for auth requests

---
+++ src/__test__/auth.test.ts
@@ -0,0 +1,102 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import supertest from "supertest";
+import { v4 as uuidv4 } from "uuid";
+import { createUserRepo } from "../services/auth.service";
+import { hashing } from "../utils/hashing";
+import createServer from "../utils/server";
+
+const app = createServer();
+
+const userAdmin = {
+    user_id: uuidv4(),
+    email: "ariefbudiman123@gmail.com",
+    name: "arief",
+    password: `${hashing("pw123")}`,
+    role: "admin",
+};
+
+const userReguler = {
+    user_id: uuidv4(),
+    email: "ariefbudiman@gmail.com",
+    name: "arief",
+    password: `${hashing("pw123")}`,
+    role: "regular",
+};
+
+const createUserAdmin = {
+    email: "arief123@gmail.com",
+    name: "arief",
+    password: "pw123",
+    role: "admin",
+};
+
+const createUserReguler = {
+    email: "arief@gmail.com",
+    name: "arief",
+    password: "pw123",
+};
+
+const loginUser = {
+    email: "ariefbudiman123@gmail.com",
+    password: "pw123",
+};
+
+const userNotExist = {
+    email: "budi@gmail.com",
+    password: "pw123",
+};
+
+describe("product", () => {
+    beforeAll(async () => {
+        const mongoServer = await MongoMemoryServer.create();
+        await mongoose.connect(mongoServer.getUri());
+        await createUserRepo(userAdmin);
+        await createUserRepo(userReguler);
+    });
+
+    afterAll(async () => {
+        await mongoose.disconnect();
+        await mongoose.connection.close();
+    });
+
+    describe("register", () => {
+        describe("create user with role admin", () => {
+            it("should return 201, success create user admin", async () => {
+                await supertest(app)
+                    .post("/auth/register")
+                    .send(createUserAdmin)
+                    .expect(201);
+            });
+        });
+
+        describe("create user with role reguler", () => {
+            it("should return 201, success create user reguler", async () => {
+                await supertest(app)
+                    .post("/auth/register")
+                    .send(createUserReguler)
+                    .expect(201);
+            });
+        });
+    });
+
+    describe("login", () => {
+        describe("login with exist user", () => {
+            it("should return 200, return access token & refresh token", async () => {
+                await supertest(app)
+                    .post("/auth/login")
+                    .send(loginUser)
+                    .expect(200);
+            });
+        });
+
+        describe("login with not exist user", () => {
+            it("should return 404, login failed", async () => {
+                await supertest(app)
+                    .post("/auth/login")
+                    .send(userNotExist)
+                    .expect(404);
+            });
+        });
+    });
+});
